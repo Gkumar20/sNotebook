3b40af4dc388820188f0721a95d42dbbbcb865b9
Ganesh Kumar <ganeshkr6754@gmail.com>
2023-03-08 17:25:51 +0530

    uselocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+
 
 function App() {
   return (
-    <h1>this is snotebook app</h1>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
